08febc23a30f08fa08e12c7b75b3934f5bd65b20
Victor Miguez <victorcineis@gmail.com>
2015-08-28 10:40:43 -0300

    fix(contador): bug de divergência entre cliques e ranking

--- demo/client/admin/admin.events.js
+++ demo/client/admin/admin.events.js
@@ -24,26 +24,6 @@ Template.admin.events({
 
     Meteor.setTimeout(function () {
       Iniciado.update(Iniciado.findOne({})._id, {$set: {jogoIniciou: false}});
-      var total = Pontos.find().fetch().length;
-
-      var usuarios = _.uniq(Pontos.find({}, {
-          sort: {userId: 1}, fields: {userId: true}
-      }).fetch().map(function(x) {
-          return x.userId;
-      }), true);
-
-      pontuacoes = [];
-
-      usuarios.forEach(function (element) {
-        pontuacoes.push({userName: Pontos.findOne({userId: element}).userName, pontos: Pontos.find({userId: element}).fetch().length});
-      });
-
-      var rankingUsuarios = pontuacoes.sort(function (a, b) {
-        return b.pontos - a.pontos;
-      })
-
-      Session.set('pontuacoes', rankingUsuarios)
-
     }, 4000)
   }
 });
--- demo/client/admin/admin.helpers.js
+++ demo/client/admin/admin.helpers.js
@@ -3,6 +3,24 @@ Template.admin.helpers({
     return Session.get('iniciado');
   },
   pontuacoes: function () {
+    var usuarios = _.uniq(Pontos.find({}, {
+      sort: {userId: 1}, fields: {userId: true}
+    }).fetch().map(function(x) {
+      return x.userId;
+    }), true);
+
+    pontuacoes = [];
+
+    usuarios.forEach(function (element) {
+      pontuacoes.push({userName: Pontos.findOne({userId: element}).userName, pontos: Pontos.find({userId: element}).fetch().length});
+    });
+
+    var rankingUsuarios = pontuacoes.sort(function (a, b) {
+      return b.pontos - a.pontos;
+    })
+
+    Session.set('pontuacoes', rankingUsuarios);
+
     return Session.get('pontuacoes');
   },
   pontosRegistrados: function () {
